4ac5a140bb2f3285ca85f712e5931cddcc8768de
Ridoy Majumdar <majumdar.ridoy@gmail.com>
2020-09-12 14:05:47 -0700

    Riff raff

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
     console.log(reaction.message);
     let discordid = reaction.message.author.id;
     let name = reaction.message.author.username;
-    let query = `INSERT INTO leader (name, discordid, count) VALUES ('${name}', '${discordid}', 1) ON CONFLICT DO UPDATE SET count = count + 1;`
+    let query = `INSERT INTO leader (name, discordid, count) VALUES ('${name}', '${discordid}', 1) ON CONFLICT (discordid) DO UPDATE SET count = count + 1;`
     console.log(query);
     pgClient.query(query, (err, res) => {
         if (err) throw err;
